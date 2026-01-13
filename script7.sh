902be2c4d658deecb46e47a2026bfbda374309a5
Graciano-B-L-Junior <gracianobarbosalimajunior@gmail.com>
2026-01-13 20:20:19 -0300

    fix: Correct target folder path and backup file location in backup script

--- script7.sh
+++ script7.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-TARGET_FOLDER="exercise 7"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/exercise\ 7 && pwd )"
+TARGET_FOLDER=important\ folder
 
 date_infix=$(date +%Y_%m_%d)
 prefix="backup"
@@ -9,7 +9,7 @@ backup_file_name="$prefix"_"$date_infix".tar.gz
 
 if [ -d "$SCRIPT_DIR/$TARGET_FOLDER" ]; then
     echo "Creating backup of $TARGET_FOLDER..."
-    tar -czvf "$backup_file_name" -C "$SCRIPT_DIR" "$TARGET_FOLDER"
+    tar -czvf "$SCRIPT_DIR/$backup_file_name" -C "$SCRIPT_DIR" "$TARGET_FOLDER"
 else
     echo "Directory $TARGET_FOLDER not found."
 fi
