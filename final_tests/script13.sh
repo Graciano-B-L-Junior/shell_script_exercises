aeb972b9c2b23d916f0e90d060cf2bed28483aa3
Graciano-B-L-Junior <gracianobarbosalimajunior@gmail.com>
2026-01-15 21:08:04 -0300

    fix: Update script13.sh to verify specific environment variables are set

--- final_tests/script13.sh
+++ final_tests/script13.sh
@@ -3,10 +3,11 @@ echo "VERIFYING IF SOME ENVIRONMENT VARIABLES ARE SET"
 echo "##########################"
 echo ""
 
-for env in DB_HOST, DB_USER, DB_PASS;
-do
-    if [[ -z "${env}" ]];then
-        echo "Environment variable $env is not set"
-        exit 1
-    fi
-done
+if [[ -z "${DB_HOST}" ]] || [[ -z "${DB_USER}" ]] || [[ -z "${DB_PASSWORD}" ]];
+then
+    echo "Some environment variables are not set"
+    exit 1
+fi
+
+echo "All environment variables are set"
+echo "" 
